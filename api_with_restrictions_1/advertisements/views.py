8a9401c90a7caa700c512c2e03a6848d69f947b1
Tahir-Ryasnov <107679690+Tahir-Ryasnov@users.noreply.github.com>
2022-12-27 08:08:45 +0300

    Add files via upload

---
+++ api_with_restrictions_1/advertisements/views.py
@@ -0,0 +1,28 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
+from rest_framework.viewsets import ModelViewSet
+
+from advertisements.filters import AdvertisementFilter
+from advertisements.models import Advertisement
+from advertisements.permissions import IsOwnerOrReadOnly
+from advertisements.serializers import AdvertisementSerializer
+from django_filters import rest_framework as filters
+
+
+class AdvertisementViewSet(ModelViewSet):
+    """ViewSet для объявлений."""
+
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    filter_backends = [filters.DjangoFilterBackend, ]
+    filter_class = [AdvertisementFilter, ]
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+    throttle_classes = [UserRateThrottle, AnonRateThrottle, ]
+
+    def get_permissions(self):
+        """Получение прав для действий."""
+
+        if self.action in ["create", "update", "partial_update"]:
+            return [IsAuthenticated()]
+
+        return []
