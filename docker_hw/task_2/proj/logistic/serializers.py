628dddc174a021370ebd7524396ad32a6fc3837d
Tahir-Ryasnov <107679690+Tahir-Ryasnov@users.noreply.github.com>
2023-02-18 14:56:54 +0700

    Add files via upload

---
+++ docker_hw/task_2/proj/logistic/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from .models import Product, StockProduct, Stock
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['title', 'description']
+
+
+class ProductPositionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
+
+
+class StockSerializer(serializers.ModelSerializer):
+    positions = ProductPositionSerializer(many=True)
+    class Meta:
+        model = Stock
+        fields = ['address', 'products', 'positions']
+
+    def create(self, validated_data):
+        positions = validated_data.pop('positions')
+        stock = super().create(validated_data)
+        for position in positions:
+            StockProduct.objects.create(stock=stock, **position)
+        return stock
+
+    def update(self, instance, validated_data):
+        positions = validated_data.pop('positions')
+        stock = super().update(instance, validated_data)
+        for position in positions:
+            StockProduct.object.update_or_create(
+                stock=stock,
+                product=position['product'],
+                defaults={'stock': stock, 'product': position['product'],
+                          'quantity': position['quantity'], 'price': position['price']})
+        return stock
